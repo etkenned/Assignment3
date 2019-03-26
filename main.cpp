aa83a9a85b9942a811667ee9fa9cba74b5676f9a
etkenned <36723249+etkenned@users.noreply.github.com>
2019-03-26 16:25:13 -0700

    added check delimiters

--- main.cpp
+++ main.cpp
@@ -3,6 +3,47 @@
 #include "Genstack.h"
 using namespace std;
 
+bool CheckDelimiters()
+{
+  string line = "";
+  int lineNum = 1;
+  fstream inputFile(fileName);
+  if(inputFile.is_open())
+  {
+    while(getline(inputFile,line))
+    {
+      for(int i = 0; i < line.length()-1; i++)
+      {
+          if(line[i] == '(' || line[i] == '[' || line[i] == '{')
+          {
+            array.push(line[i]);
+          }
+          else if(line[i] == ')' || line[i] == ']' || line[i] == '}')
+          {
+            T temp = array.pop();
+            if(temp != line[i])
+            {
+              cout << "Line " << lineNum << ": expected " << "PLACE HOLDER" << " and found " << line[i] << endl; 
+              //alert the user to the location of the problem
+              return false;
+            }
+          }
+      }
+    }
+    if(array.isEmpty())
+    {
+      cout << "Delimiter syntax is correct" << endl;
+      return true;
+    }
+    else
+    {
+      
+    }
+    
+  }
+}
+
+
 int main(int argc, char** argv)
 {
   string fileName = "";
