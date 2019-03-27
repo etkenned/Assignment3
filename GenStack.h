05dd7842cdd635f530e1e4d68a0da0cbb6bda63c
etkenned <36723249+etkenned@users.noreply.github.com>
2019-03-26 20:02:00 -0700

    compiles

--- GenStack.h
+++ GenStack.h
@@ -22,3 +22,71 @@ class GenStack
     int top;
     T *array;
 };
+template <class T>
+GenStack<T>::GenStack()
+{
+  array = new T[10];
+  size = 10;
+  top = -1;
+}
+template <class T>
+GenStack<T>::GenStack(int arraySize)
+{
+  array = new T[arraySize];
+  size = arraySize;
+  top = -1;
+}
+template <class T>
+GenStack<T>::~GenStack()
+{
+
+}
+template <class T>
+void GenStack<T>::push(T object)
+{
+  if(top == size - 1)//checks if the stack is full
+  {
+    T *tempArray;
+    tempArray = new T[size * 2]; //doubles the stack size
+    for(int i = 0; i < size - 1; i++)
+    {
+      tempArray[i] = array[i];
+    }
+    array = tempArray;
+    size = size * 2;
+  }
+  array[top++] = object; //adds object to the stack
+}
+template <class T>
+T GenStack<T>::pop()
+{
+  if(isEmpty())//checks if the array is empty
+  {
+    return '0'; // exception *********************************************************
+  }
+  else
+  {
+    T temp = array[top--];
+    return temp;
+  }
+}
+template <class T>
+T GenStack<T>::peak()
+{
+  return array(top);
+}
+template <class T>
+int GenStack<T>::getSize()
+{
+  return size;
+}
+template <class T>
+bool GenStack<T>::isEmpty()
+{
+  return (top == -1);
+}
+template <class T>
+bool GenStack<T>::isFull()
+{
+  return (top == size - 1);
+}
--- main.cpp
+++ main.cpp
@@ -1,34 +1,57 @@
 #include <iostream>
 #include <fstream>
-#include "Genstack.h"
+#include "GenStack.h"
+#include <string>
 using namespace std;
 
-bool CheckDelimiters()
+
+bool CheckDelimiters( string fileName)
 {
+  GenStack<char> array = GenStack<char>();
   string line = "";
   int lineNum = 1;
   fstream inputFile(fileName);
   if(inputFile.is_open())
   {
-    while(getline(inputFile,line))
+    while(getline(inputFile,line)) // gets ever line in the file
     {
-      for(int i = 0; i < line.length()-1; i++)
+      for(int i = 0; i < line.length()-1; i++) //iterates through the char of the line
       {
-          if(line[i] == '(' || line[i] == '[' || line[i] == '{')
+          if(line[i] == '(' || line[i] == '[' || line[i] == '{') //checks for opening bracket
           {
             array.push(line[i]);
           }
-          else if(line[i] == ')' || line[i] == ']' || line[i] == '}')
+          else if(line[i] == ')' || line[i] == ']' || line[i] == '}')//checks for closeing bracket
           {
-            T temp = array.pop();
-            if(temp != line[i])
+            if(array.isEmpty())
             {
-              cout << "Line " << lineNum << ": expected " << "PLACE HOLDER" << " and found " << line[i] << endl; 
-              //alert the user to the location of the problem
+              cout << "Line " << lineNum << ": there is a " << line[i] << " with no open delimiter" << endl;
               return false;
+            }
+            else
+            {
+              char temp = array.pop();
+
+              if(temp == '(' && line[i] != ')')
+              {
+                cout << "Line " << lineNum << ": expected )" << " and found " << line[i] << endl; //alert the user to the location of the problem
+                return false;
+              }
+              else if(temp == '[' && line[i] != ']')
+              {
+                cout << "Line " << lineNum << ": expected ]" << " and found " << line[i] << endl; //alert the user to the location of the problem
+                return false;
+              }
+              else if(temp == '{' && line[i] != '}')
+              {
+                cout << "Line " << lineNum << ": expected }" << " and found " << line[i] << endl; //alert the user to the location of the problem
+                return false;
+              }
+
             }
           }
       }
+      lineNum++;
     }
     if(array.isEmpty())
     {
@@ -37,12 +60,18 @@ bool CheckDelimiters()
     }
     else
     {
-      
+      cout << "Some delimiters were never matched" << endl;
+      return false;
     }
-    
-  }
-}
 
+  }
+  else // if the file name does not work
+  {
+    cout << "Error in name of file" << endl;
+    return true;
+  }
+  inputFile.close();
+}
 
 int main(int argc, char** argv)
 {
@@ -51,7 +80,23 @@ int main(int argc, char** argv)
   if(argc > 1) // checks for user input of file name
   {
     fileName = argv[1];
-
   }
-  fstream inputFile(fileName);
+  string input = "";
+  while(CheckDelimiters(fileName))
+  {
+    cout << "Would you like to analyze another file? (y) or (n)" << endl;
+    cin >> input;
+    if(input == "n")
+    {
+      break;
+    }
+    else if(input == "y")
+    {
+    }
+    else
+    {
+      cout << "invalid input, ending program." << endl;
+      break;
+    }
+  }
 }
